22a8ed066a223be025fdc753e652a1339280db87
Nicholas Still <nickstill93@gmail.com>
2025-11-17 11:17:02 -0800

    fragile working append
    Creating a builder using a file writer from the file path given by the
    archive argument, then seeking that to the end minus 1024 bytes based on
    the 2 empty blocks specified by the tar format seems fragile to me. But
    it works for now.

--- src/uu/tar/src/operations/append.rs
+++ src/uu/tar/src/operations/append.rs
@@ -1,8 +1,9 @@
 use uucore::error::UResult;
-use crate::options::TarParams;
+use crate::options::{TarOption, TarParams};
 use crate::operations::TarOperation;
-use std::fs::File;
-use tar::{Archive, Builder};
+use std::fs::{OpenOptions, File};
+use std::io::{Seek, SeekFrom};
+use tar::{Archive, Builder, Header};
 
 pub struct Append;
 
@@ -11,7 +12,15 @@ impl TarOperation for Append {
 
         // NOTE: might have to seek reader to end of entries
         // Then write the entry
-        let mut archive = Archive::new(File::open(params.archive())?);
+        let mut f = OpenOptions::new().write(true).open(params.archive())?;
+        let mut builder = Builder::new(f);
+        // seek to end minus 2 blocks for empty
+        builder.get_mut().seek(SeekFrom::End(-1024))?;
+        for file in params.files() {
+            let mut ff = File::open(file)?;
+            builder.append_file(file, &mut ff)?;
+        }
+        builder.into_inner()?;
         Ok(()) 
     }
 }
